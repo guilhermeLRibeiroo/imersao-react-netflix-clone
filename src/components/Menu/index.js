17154d2b5b9f8cd8497a95385041d31bd6107efd
guilhermeLRibeiroo <guilazribeiro@gmail.com>
2020-07-30 09:24:32 -0300

    Adicionado paginas de cadastro de categoria e de video e react router dom

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,13 +3,15 @@ import LogoMain from '../../assets/img/LogoMain.png';
 import './Menu.css';
 import Button from '../Button';
 
+import { Link } from 'react-router-dom'; 
+
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={LogoMain} alt="AluraFlix logo" />
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
